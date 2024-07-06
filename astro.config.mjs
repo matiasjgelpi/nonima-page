18cf9089612781bcedc342c764159d9207f78576
matiasjgelpi <129074171+matiasgelpi@users.noreply.github.com>
2024-07-06 12:08:36 -0300

    wip new layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({ integrations: [react()] });
+export default defineConfig({
+  integrations: [react(), tailwind()]
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["Quicksand Variable", "sans-serif"],
+      heading: ["League Spartan", "sans-serif"],
+    },
+    colors: {
+      mati: "#FFFFFF",
+      black: "#000",
+      green: "#438185",
+      violet: "#8963ba",
+      "light-green": "rgba(183, 215, 216, 0.8)",
+      "light-violet": "rgba(164, 149, 193, 0.8)",
+      "lighter-green": "rgba(183, 215, 216, 0.5)",
+      "lighter-violet": "rgba(164, 149, 193, 0.5)",
+    },
+  },
+  plugins: [],
+};
