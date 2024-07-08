1220fbffde54ac1328a4809544f581344d9cf967
matiasjgelpi <matiasjgelpi@gmail.com>
2024-07-08 11:49:58 -0300

    wip new layout

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,20 +1,16 @@
-// .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        printWidth: 80,
-        tabWidth: 2,
-        useTabs: false,
-        semi: true,
-        singleQuote: true,
-        trailingComma: "all",
-        bracketSpacing: true,
-        jsxBracketSameLine: false,
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+         
+
+        },
       },
-    },
-  ],
-};
+    ],
+ 
+
+  };
