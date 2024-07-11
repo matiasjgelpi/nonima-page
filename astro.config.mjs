9967d74f8308ff80ccf11a10c2e6721bce2fb081
matiasjgelpi <matiasjgelpi@gmail.com>
2024-07-11 14:36:52 -0300

    adding adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
-  adapter: vercel()
+  adapter: netlify()
 });
